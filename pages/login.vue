b34a99d14477379117626fbc198ab3f6dacebc8e
Mel <hungle@beeknights.com>
2023-03-27 18:09:46 +0700

    refactor code

--- pages/login.vue
+++ pages/login.vue
@@ -2,24 +2,19 @@
 import { useQuasar } from 'quasar'
 import { userRepo } from '~~/repository/user'
 
-// import { userRepo } from '../repository/user'
 definePageMeta({
   layout: 'no-auth'
 })
 
 const email = ref('')
 const password = ref('')
-const config = useRuntimeConfig()
 const $q = useQuasar()
 
 const handleSubmitForm = async () => {
   try {
-    console.log(config)
     const res = await userRepo().login('studentnew@gmail.com', '1231233')
     console.log(res)
   } catch (error: any) {
-    console.log(error)
-    console.log($q)
     $q.notify({
       message: error.message
     })
@@ -36,12 +31,19 @@ const handleSubmitForm = async () => {
       class="grid grid-cols-3 gap-5 w-full max-w-[700px] mx-auto translate-y-[-100px]"
     >
       <h2 class="text-center col-span-3">Login</h2>
-      <q-input v-model="email" label="Email" outlined class="col-span-3" />
+      <q-input
+        v-model="email"
+        label="Email"
+        outlined
+        class="col-span-3"
+        :rules="[required, validateEmail]"
+      />
       <q-input
         v-model="password"
         outlined
         label="Password"
         class="col-span-3"
+        :rules="[required]"
         type="password"
       />
       <q-btn @click="$router.push('/')" class="col-span-1 mt-2">
